952f2e1f7475c4d3250215d59d8404779f36241c
Sergey Tkachenko <serg.tkachenko@hotmail.com>
2016-10-23 15:26:29 +0300

    Initial Commit

---
+++ chess-visual/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  chess-visual
+//
+//  Created by Segey Tkachenko on 23.10.16.
+//  Copyright © 2016 Segey Tkachenko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
